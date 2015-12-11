ba1775bbf3a2c120df203e017f7ee97ebbe14d49
Alec Iverson <adiverso93@gmail.com>
2015-12-11 13:12:24 -0700

    Change menu hover to animate letter spacing
    Rather than move the word

--- js/index.js
+++ js/index.js
@@ -1,23 +1,20 @@
 var main = function()
 {
 	const AnimTime = 100;
-
-	var menu = $("#header #menu");
-	var diff = menu.width() - $("#header #menu a").width();
-
+	
 	$("#header #menu a").hover(
 		function()
 		{
 			$(this).animate(
 			{
-				paddingLeft: diff + "px"
+				letterSpacing: "1em"
 			}, AnimTime);
 		},
 		function()
 		{
 			$(this).animate(
 			{
-				paddingLeft: "0px"
+				paddingLeft: "normal"
 			}, AnimTime);
 		}
 	);
